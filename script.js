c471877a50517e47b40d7d00885534b30fcf83f1
Akash M <108210779+akashikur@users.noreply.github.com>
2023-06-26 19:22:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
-function threeSum(arr, target) {
+function threeSum(nums, target) {
 // write your code here
+	nums.sort((a, b) => a - b);
+
+  let closestSum = Infinity;
+  let minDiff = Infinity;
+
+  // Iterate through each number as the potential first element
+  for (let i = 0; i < nums.length - 2; i++) {
+    let left = i + 1;
+    let right = nums.length - 1;
+
+    while (left < right) {
+      const sum = nums[i] + nums[left] + nums[right];
+      const diff = Math.abs(sum - target);
+
+      // Check if the current sum is closer to the target than the previous closest sum
+      if (diff < minDiff) {
+        closestSum = sum;
+        minDiff = diff;
+      }
+
+      // Adjust the pointers based on the current sum
+      if (sum < target) {
+        left++;
+      } else if (sum > target) {
+        right--;
+      } else {
+        // If the sum is equal to the target, return the target sum
+        return sum;
+      }
+    }
+  }
+
+  return closestSum;
   
 }
 
